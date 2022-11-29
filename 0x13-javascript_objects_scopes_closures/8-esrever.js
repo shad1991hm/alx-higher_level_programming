8c20f76c56e546b1b037213b2cd8e5cc79868556
shad1991hm <shad1991hm@gmail.com>
2022-11-29 21:31:41 +0000

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+// function that returns the reversed version of a list
 
 exports.esrever = function (list) {
 
-	let rlist = [];
-	while (list.length > 0) {
-		rlist.push(list.pop());
-	}
-	return rlist;
+  let rlist = [];
+  while (list.length > 0) {
+    rlist.push(list.pop());
+  }
+  return rlist;
 };
