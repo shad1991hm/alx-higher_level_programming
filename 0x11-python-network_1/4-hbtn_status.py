7da495e4466bf6b4542e16b5dc8ed24a62c24c9e
shad1991hm <shad1991hm@gmail.com>
2022-12-16 17:32:38 -0800

    net 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
+    print("\t- type: {}".format(r.text.__class__))
     print("\t- content: {}".format(r.text))
